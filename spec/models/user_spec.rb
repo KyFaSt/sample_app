a97bbb0e0af35e85317a761f5d3426073b616c4b
kstradley <kylie.stradley@gmail.com>
2013-10-24 21:51:17 -0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -83,7 +83,7 @@ describe User do
   		it { should be_invalid }
   	end
 
-  	describe "retunr value of authenticate method" do
+  	describe "return value of authenticate method" do
   		before { @user.save }
   		let (:found_user) { User.find_by(email: @user.email) }
 
